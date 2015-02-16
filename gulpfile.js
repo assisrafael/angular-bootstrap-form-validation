85d19cac5eac189e15c020b496ab34ced00ec006
Igor Rafael <igor.rafael@gmail.com>
2015-02-16 16:13:59 +0100

    chore: gulp jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var changelog = require('conventional-changelog');
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var karma = require('karma').server;
 var path = require('path');
 var pkg = require('./package.json');
 
-gulp.task('changelog', function(done) {
+gulp.task('changelog', function() {
 	var options = {
 		reporitory: pkg.homepage,
 		version: pkg.version,
@@ -14,8 +15,28 @@ gulp.task('changelog', function(done) {
 
 	var filePath = path.join(__dirname, options.file);
 	changelog(options, function(err, log) {
+		if (err) {
+			throw err;
+		}
+
 		fs.writeFileSync(filePath, log);
 	});
+});
+
+gulp.task('jshint', function() {
+	gulp.src([
+		'src/**/*.js',
+		'config/karma.conf.js',
+		'gulpfile.js'
+	])
+	.pipe(jshint('.jshintrc'))
+	.pipe(jshint.reporter(require('jshint-summary'), {
+		fileColCol: ',bold',
+		positionCol: ',bold',
+		codeCol: 'green,bold',
+		reasonCol: 'cyan'
+	}))
+	.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', function(done) {
