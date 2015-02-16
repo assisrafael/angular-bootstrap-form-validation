bf53c579516d4da0105e6aed02bd985aa6fb1603
Igor Rafael <igor.rafael@gmail.com>
2015-02-16 10:36:44 +0100

    chore: conventional changelog

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp'),
-	karma = require('karma').server;
+var fs = require('fs');
+var changelog = require('conventional-changelog');
+var gulp = require('gulp');
+var karma = require('karma').server;
+var path = require('path');
+var pkg = require('./package.json');
+
+gulp.task('changelog', function(done) {
+	var options = {
+		reporitory: pkg.homepage,
+		version: pkg.version,
+		file: 'CHANGELOG.md'
+	};
+
+	var filePath = path.join(__dirname, options.file);
+	changelog(options, function(err, log) {
+		fs.writeFileSync(filePath, log);
+	});
+});
 
 gulp.task('test', function(done) {
 	var karmaConfig = {
